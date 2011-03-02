5015d8d25e2949642d29ad61fa9f04caadeee475
Alex Koppel <alex+git@alexkoppel.com>
2011-03-02 11:36:18 +0100

    Fixed indentation.

--- uploader.php
+++ uploader.php
@@ -7,8 +7,6 @@ define("PRODUCTION", preg_match('/alexkoppel\.com/', $_SERVER['SERVER_NAME']));
 define("IMAGE_URL_STEM", (PRODUCTION ? "http://images.alexkoppel.com/" : "http://10.0.1.7/~ahkoppel2/uploader/images/"));
 define("IMAGE_PATH", dirname(__FILE__) . "/images/");
 
-error_log("Starting");
-
 // max size: 4 MB
 define("MAX_FILESIZE", 5000);
 
@@ -24,24 +22,24 @@ function resizeImage($image, $maxDimension, $destinationFilename, $path, $origin
     $isHorizontal = $originalHeight < $originalWidth;
 
     if ($isHorizontal) {
-       $newHeight = ($originalHeight / $originalWidth) * $maxDimension;
-       $newWidth = $maxDimension;
-     }
-     else {
-       $newWidth = ($originalWidth / $originalHeight) * $maxDimension;
-       $newHeight = $maxDimension;
-     }
-     
-     $tmp = imagecreatetruecolor($newWidth, $newHeight);
+        $newHeight = ($originalHeight / $originalWidth) * $maxDimension;
+        $newWidth = $maxDimension;
+    }
+    else {
+        $newWidth = ($originalWidth / $originalHeight) * $maxDimension;
+        $newHeight = $maxDimension;
+    }
 
-     imagecopyresampled($tmp, $image, 0, 0, 0, 0, $newWidth, $newHeight, $originalWidth, $originalHeight);
+    $tmp = imagecreatetruecolor($newWidth, $newHeight);
 
-     $newImageLocation = $path . $destinationFilename;
+    imagecopyresampled($tmp, $image, 0, 0, 0, 0, $newWidth, $newHeight, $originalWidth, $originalHeight);
 
-     $g = imagejpeg($tmp, $newImageLocation, 100);
- 
-     imagedestroy($tmp);
-     return array("width" => (int)$newWidth, "height" => (int)$newHeight);
+    $newImageLocation = $path . $destinationFilename;
+
+    $g = imagejpeg($tmp, $newImageLocation, 100);
+
+    imagedestroy($tmp);
+    return array("width" => (int)$newWidth, "height" => (int)$newHeight);
 }
 
 $results = array();
@@ -55,18 +53,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // need to handle files too large!
     error_log("Request to handle " . $image_name);
     error_log("Files: " . print_r($_FILES, true));
-    
+
     if ($image_name) {
         $filename = stripslashes($image_name);
         $extension = strtolower(getExtension($filename));
-		
+
         if (($extension != "jpg") && ($extension != "jpeg") && ($extension != "png") && ($extension != "gif")) {
             $error = 'Unknown image extension ' . $extension;
         }
         else {
             $size = filesize($tmp_file);
             if ($size > MAX_FILESIZE * 1024) {
-				$error = "Size limit of " . MAX_FILESIZE . "KB exceeded.";
+                $error = "Size limit of " . MAX_FILESIZE . "KB exceeded.";
             }
             else {
                 if ($extension == "jpg" || $extension == "jpeg" ) {
@@ -82,14 +80,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 // set up a random, unique filename
                 list($originalWidth, $originalHeight) = getimagesize($tmp_file);
                 $result_filename_stem = date("YmdHi_" . rand());
-                
+
                 foreach ($thumbnails as $image_type => $details) {
                     $newFilename = $result_filename_stem . "_" . $image_type . ".jpg";
                     $imageResult = array("url" => IMAGE_URL_STEM . $newFilename);
                     $imageResult = array_merge($imageResult, resizeImage($src, $details["size"], $newFilename, IMAGE_PATH, $originalWidth, $originalHeight));
                     $results[$image_type] = $imageResult;
                 }
-                
+
                 $full_filename = $result_filename_stem . "_full.jpg";
                 copy($tmp_file, IMAGE_PATH . $full_filename);
                 $results["full"] = array(
@@ -97,13 +95,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     "size" => $size, 
                     "width" => $originalWidth, 
                     "height" => $originalHeight
-                );
-                
+                    );
+
                 // get rid of the original temp file
                 imagedestroy($src);
             }
         }
-        
+
         if ($error) {
             echo json_encode(array("error" => $error));
         }
